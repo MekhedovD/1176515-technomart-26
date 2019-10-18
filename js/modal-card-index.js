b8e41a390d83c03769647e434d5e7fe30b9c53c1
MekhedovD <mekhedov.d@mail.ru>
2019-10-18 18:33:07 +0300

    консультация по ИЕ

---
+++ js/modal-card-index.js
@@ -0,0 +1,43 @@
+var orderLink = document.querySelector(".checkout-order");
+var orderPopup = document.querySelector(".modal-order-card");
+var orderClose = document.querySelector(".modal-close-card");
+
+orderLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  orderPopup.classList.add("modal-show");
+});
+orderClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  orderPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (orderPopup.classList.contains("modal-show")) {
+      orderPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+var orderLink = document.querySelector(".checkout-order");
+  var orderPopup = document.querySelector(".modal-order-card");
+  var orderClose = document.querySelector(".modal-close");
+
+  orderLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    orderPopup.classList.add("modal-show");
+});
+orderClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    orderPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (orderPopup.classList.contains("modal-show")) {
+        orderPopup.classList.remove("modal-show");
+      }
+    }
+  });
